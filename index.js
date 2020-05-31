f613b6e95666f51d8c1a6e069a76a7e3ee461fa7
Mike Preacher <44718469+mpreacher23@users.noreply.github.com>
2020-05-31 16:37:48 -0500

    Updates to Index.js, infoReadme, License, etc.

--- index.js
+++ index.js
@@ -2,22 +2,66 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const util = require("util");
 
 // Questions asked to user
 const userQuestions = require("userQuestions");
+
+// Need to seperate markdown code and make it a template file 
 const createMarkdown = require("createMarkdown");
 
+// createMarkdown Function
+function createMarkdown(answers, data) {
+    return `  
+    ${answers.tittle}
+    ${answers.banner}
+
+    ## Description\n
+    ${answers.description}
+
+    ## Installation
+    ${answers.install}
+    
+    ## License
+    ${answers.license}
+
+    ## Usage
+    ${answers.usage}
+
+    ## Contributors
+    ${answers.contributors}
+
+    ## Tests
+    ${answers.tests}
+
+    ## Contact
+    ${answers.email}
+
+    ## UserName
+    ${answers.username}
+`;
+}
 
 
 // Prompts the user to enter the details of the project
 function promptUser() {
-    return inquirer.prompt(questions);
+    return inquirer.prompt(userQuestions);
 };
 
+//
+const createReadmeAsync = util.promisfy(fs.createReadme);
 
-assync function
+async function init() {
 
-
+    try {
+        const answers = await promptUser();
+        let github = await axios.get(`https://api.github.com/users/${answers.username}`)
+        const md = createMarkdown(answers, github.data);
+        await createReadmeAsync('README.md", md);
+        console.log("You just created your projects README.md file!");
+    }
+};
+init();
 
 
 // userQuestions Prompts (Tittle, Banner Img., Description, Installation, Usage, License, Contributors, Tests, GitHub Email, GitHub UserName )
@@ -62,8 +106,10 @@ const userQuestions = [
     },
     {
         message: "What is your GitHub username?",
+        name: "username"
     }
 ];
+// module.exports = questionsAsked;
 
 
 // Gen. Markdown Function
---
+++ resources.md
@@ -0,0 +1,24 @@
+<!-- **** Resrou -->
+
+<!-- Using Axios NPM with Node.js-->
+https://www.npmjs.com/package/axios
+
+<!-- NPM Util -->
+https://nodejs.org/api/util.html
+
+<!-- Async functions -->
+https://bytearcher.com/articles/asynchronous-call-in-constructor/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+
+
+
+<!-- Proper markdown guide -->
+https://guides.github.com/features/mastering-markdown/
+
+<!-- Choosing a license type -->
+https://choosealicense.com/licenses/mit/#
+
+<!-- Badges and how to use them -->
+https://shields.io/
+
+My badge-  https://img.shields.io/github/license/mpreacher23/W9SMU-Node.js?style=social
