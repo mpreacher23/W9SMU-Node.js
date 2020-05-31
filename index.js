e351285da801d54af9f154ffa65c3fb789ece495
Mike Preacher <44718469+mpreacher23@users.noreply.github.com>
2020-05-31 14:25:51 -0500

    updates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const questions = require("questions");
+const genMarkdown = require("genMarkdown");
 
+function promptUser() {
+    return inquirer.prompt(questions);
+};
+
+
+assync function 
