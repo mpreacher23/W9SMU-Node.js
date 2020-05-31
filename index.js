6787395d653f2b2ad7ebb8e929f0634fd24cf915
Mike Preacher <44718469+mpreacher23@users.noreply.github.com>
2020-05-31 15:35:14 -0500

    Update to index.js code

--- index.js
+++ index.js
@@ -1,12 +1,69 @@
+// Dependencies 
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const questions = require("questions");
-const genMarkdown = require("genMarkdown");
 
+// Questions asked to user
+const userQuestions = require("userQuestions");
+const createMarkdown = require("createMarkdown");
+
+
+
+// Prompts the user to enter the details of the project
 function promptUser() {
     return inquirer.prompt(questions);
 };
 
 
-assync function 
+assync function
+
+
+
+
+// userQuestions Prompts (Tittle, Banner Img., Description, Installation, Usage, License, Contributors, Tests, GitHub Email, GitHub UserName )
+const userQuestions = [
+    {
+        message: "what is the Tittle of your project? ",
+        name: "tittle"
+    },
+    {
+        message: "Would you like a banner? (Add your banner link here):",
+        name: "banner"
+    },
+    {
+        message: "What is your projects description?",
+        name: "description"
+    },
+    {
+        message: "What command should be used to Install and Run your App?",
+        name: "install"
+    },
+    {
+        message: "Define what usage this repo can be used for",
+        name: "usage"
+    },
+    {
+        message: " What license would you like to use?",
+        name: "license",
+        type: "list",
+        choices: ["mit", "apache-2.0", "gpl", "other", "none"]
+    },
+    {
+        message: "Who contributed to your project?",
+        name: "contributors"
+    },
+    {
+        message: "Did you run any Tests?",
+        name: "tests"
+    },
+    {
+        message: "What is your GitHub Email?",
+        name: "email"
+    },
+    {
+        message: "What is your GitHub username?",
+    }
+];
+
+
+// Gen. Markdown Function
