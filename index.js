5e398274b1322aa9dce042e42bd0fd5b8c428828
Mike Preacher <44718469+mpreacher23@users.noreply.github.com>
2020-05-31 22:14:34 -0500

    Update to all files. Working but needs correction

--- index.js
+++ index.js
@@ -5,51 +5,18 @@ const inquirer = require("inquirer");
 const util = require("util");
 
 // Questions asked to user
-const userQuestions = require("userQuestions");
+const userQuestions = require("./userQuestions");
 
 // Need to seperate markdown code and make it a template file 
-const createMarkdown = require("createMarkdown");
-
-// createMarkdown Function
-function createMarkdown(answers, data) {
-    return `  
-    ${answers.tittle}
-    ${answers.banner}
-
-    ## Description\n
-    ${answers.description}
-
-    ## Installation
-    ${answers.install}
-    
-    ## License
-    ${answers.license}
-
-    ## Usage
-    ${answers.usage}
-
-    ## Contributors
-    ${answers.contributors}
-
-    ## Tests
-    ${answers.tests}
-
-    ## Contact
-    ${answers.email}
-
-    ## UserName
-    ${answers.username}
-`;
-}
-
+const createMarkdown = require("./createMarkdown");
 
 // Prompts the user to enter the details of the project
 function promptUser() {
     return inquirer.prompt(userQuestions);
 };
 
-//
-const createReadmeAsync = util.promisfy(fs.createReadme);
+// Write file node.js Util, Async
+const writeFileAsync = util.promisify(fs.writeFile);
 
 async function init() {
 
@@ -57,59 +24,28 @@ async function init() {
         const answers = await promptUser();
         let github = await axios.get(`https://api.github.com/users/${answers.username}`)
         const md = createMarkdown(answers, github.data);
-        await createReadmeAsync('README.md", md);
-        console.log("You just created your projects README.md file!");
-    }
+        await writeFileAsync("README.md", md);
+        console.log("Check out your new README.md file!");
+
+    } 
 };
+
 init();
 
+// async function init() {
 
-// userQuestions Prompts (Tittle, Banner Img., Description, Installation, Usage, License, Contributors, Tests, GitHub Email, GitHub UserName )
-const userQuestions = [
-    {
-        message: "what is the Tittle of your project? ",
-        name: "tittle"
-    },
-    {
-        message: "Would you like a banner? (Add your banner link here):",
-        name: "banner"
-    },
-    {
-        message: "What is your projects description?",
-        name: "description"
-    },
-    {
-        message: "What command should be used to Install and Run your App?",
-        name: "install"
-    },
-    {
-        message: "Define what usage this repo can be used for",
-        name: "usage"
-    },
-    {
-        message: " What license would you like to use?",
-        name: "license",
-        type: "list",
-        choices: ["mit", "apache-2.0", "gpl", "other", "none"]
-    },
-    {
-        message: "Who contributed to your project?",
-        name: "contributors"
-    },
-    {
-        message: "Did you run any Tests?",
-        name: "tests"
-    },
-    {
-        message: "What is your GitHub Email?",
-        name: "email"
-    },
-    {
-        message: "What is your GitHub username?",
-        name: "username"
-    }
-];
-// module.exports = questionsAsked;
+//     try {
+//         const answers = await promptUser();
+//         let github = await axios.get(`https://api.github.com/users/${answers.username}`)
+//         const md = createMarkdown(answers, github.data);
+//         await writeFileAsync('README.md", md);
+//         console.log("You just created your projects README.md file!");
+//     }
+// };
+// init();
 
 
-// Gen. Markdown Function
+
+
+
+
--- resources.md
+++ resources.md
@@ -1,7 +1,10 @@
-<!-- **** Resrou -->
+<!-- **** Resources -->
 
 <!-- Using Axios NPM with Node.js-->
 https://www.npmjs.com/package/axios
+
+<!-- Moduler exports -->
+https://stackabuse.com/how-to-use-module-exports-in-node-js/
 
 <!-- NPM Util -->
 https://nodejs.org/api/util.html
@@ -9,8 +12,7 @@ https://nodejs.org/api/util.html
 <!-- Async functions -->
 https://bytearcher.com/articles/asynchronous-call-in-constructor/
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-
-
+https://www.codota.com/code/javascript/functions/fs/writeFileAsync
 
 <!-- Proper markdown guide -->
 https://guides.github.com/features/mastering-markdown/
